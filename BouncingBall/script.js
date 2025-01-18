105c66b7ff1cdd206a5fa0190248c5f12d6326d1
AstroB20 <114848052+AstroB20@users.noreply.github.com>
2025-01-19 00:03:04 +0530

    u

--- BouncingBall/script.js
+++ BouncingBall/script.js
@@ -9,32 +9,21 @@ const canvas = document.getElementById("bouncingBallCanvas");
         dy: 0.7,
         color: "black",
       };
-
-      // Animation function
       function animate() {
-        // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Draw the ball
         ctx.beginPath();
         ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
         ctx.fillStyle = ball.color;
         ctx.fill();
         ctx.closePath();
-
-        // Update ball position
         ball.x += ball.dx;
         ball.y += ball.dy;
-
-        // Check for collisions with walls
         if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
           ball.dx = -ball.dx;
-          // Toggle color between black and grey
           ball.color = ball.color === "black" ? "pink" : "black";
         }
         if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
           ball.dy = -ball.dy;
-          // Toggle color between black and grey
           ball.color = ball.color === "black" ? "pink" : "black";
         }
 
